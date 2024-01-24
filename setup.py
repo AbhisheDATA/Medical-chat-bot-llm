aa5ce0acc5db6b4be9a285dd153bb718f20a9b1b
Abhishek <abhisheklinkd5@gmail.com>
2024-01-24 22:39:37 +0530

    setup and dependency

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Abhishek kumar',
+    author_email= 'abhisheklinkd5@gmail.com',
+    author_url='https://github.com/AbhisheDATA/Medical-chat-bot-llm',
+    packages= find_packages(),
+    install_requires = []
+
+)
